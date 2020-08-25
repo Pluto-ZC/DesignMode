08b3b89ed07b7fd23cc1312eac12769f49a06828
Pluto-ZC <wzc19961013@163.com>
2020-08-25 10:09:15 +0800

    设计模式

---
+++ src/singleton7/Singleton7.java
@@ -0,0 +1,26 @@
+package singleton7;
+
+
+public class Singleton7 {
+
+    public static void main(String[] args) {
+        //测试
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+        System.out.println(instance1 == instance2);//true
+    }
+}
+
+//静态内部类
+class Singleton {
+    private static volatile Singleton instance;
+    public Singleton() {  }
+    //写一个静态内部类，该类中有一个静态属性Singleton
+    private static class SingletonInstance{
+        private static final Singleton INSTANCE = new Singleton();
+    }
+    //提供一个静态共有方法，直接返回 SingletonInstance.INSTANCE
+    public static synchronized Singleton getInstance() {
+        return SingletonInstance.INSTANCE;
+    }
+}
